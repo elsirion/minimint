e443ea375575a442b73861955aebf32e56ae6caf
elsirion <elsirion@protonmail.com>
2023-02-28 18:46:00 +0100

    Step 1: Add dummy module

--- fedimintd/src/bin/main.rs
+++ fedimintd/src/bin/main.rs
@@ -2,5 +2,9 @@ use fedimintd::fedimintd::Fedimintd;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Fedimintd::new()?.with_default_modules().run().await
+    Fedimintd::new()?
+        .with_default_modules()
+        .with_module(fedimint_dummy::DummyConfigGenerator)
+        .run()
+        .await
 }
